e5dd1167aa7e729d82302c7f5038b719e66629d6
likitha <101236598+LikithasriChinthapalli@users.noreply.github.com>
2022-04-19 23:01:09 +0530

    Add files via upload

---
+++ arrayint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+      int n,m,i,j,sum=0; 
+    scanf("%d %d",&n,&m);
+    int a[100];
+    for(i = 0;i<n;i++){
+       scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++) {
+            if((a[i]+a[j])%m==0)
+                sum++;
+        }
+    }
+    printf("%d\n",sum);
+    return 0;
+}
